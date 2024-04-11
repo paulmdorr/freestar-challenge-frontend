5bf9d5a820f5d1799358c75fc326ea9997ec1e7e
Paul M Dorr <paul.mdorr@gmail.com>
2024-04-10 23:46:33 -0300

    add test coverage support + fix config

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   component: {
     devServer: {
-      framework: "next",
-      bundler: "webpack",
+      framework: 'next',
+      bundler: 'webpack',
+    },
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config);
+
+      return config;
     },
   },
 });
